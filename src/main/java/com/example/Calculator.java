1b8f955cbcb3965acd2f50a2a2d8cfd0f1ed5390
Nitin Kumar <nitin.kumar@91mobiles.com>
2025-10-31 14:12:17 +0530

    test passed for 2 numbers delimited by comma

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -4,6 +4,10 @@ public class Calculator {
     public static int add(String numbers) {
         if(numbers.isEmpty())
             return 0;
+        else if (numbers.contains(",")) {
+            String tokens[] = numbers.split(",");
+            return Integer.parseInt(tokens[0]) + Integer.parseInt(tokens[1]);
+        }
         return Integer.parseInt(numbers);
     }
 }
