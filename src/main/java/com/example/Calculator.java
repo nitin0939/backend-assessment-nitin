c7b533877dfb791279dcbdf29daeddc161d24643
Nitin Kumar <nitin.kumar@91mobiles.com>
2025-11-01 00:02:50 +0530

    test passed for passing custom delimiter

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -4,14 +4,25 @@ public class Calculator {
     public static int add(String numbers) {
         if(numbers.isEmpty())
             return 0;
-        else if (numbers.contains(",")) {
-            String tokens[] = numbers.split(",|\n");
-            int sum = 0;
-            for(String token:tokens){
-                sum += Integer.parseInt(token);
-            }
-            return sum;
+
+        String[] tokens = getTokens(numbers);
+        int sum = 0;
+        for(String token : tokens) {
+            sum += Integer.parseInt(token);
         }
-        return Integer.parseInt(numbers);
+        return sum;
+    }
+
+    private static String[] getTokens(String numbers) {
+        String delimiter = ",|\n";
+
+        if(numbers.startsWith("//")) {
+            int delimiterEnd = numbers.indexOf("\n");
+            delimiter = numbers.substring(2, delimiterEnd);
+            numbers = numbers.substring(delimiterEnd + 1);
+        }
+
+        String[] tokens = numbers.split(delimiter);
+        return tokens;
     }
 }
